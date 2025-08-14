110fabc5e61183f2c4ab41aa1f662fa3e18d4f70
Ranjan Shrestha <ranjan@Pro.lan>
2025-08-14 17:44:29 -0500

    updated code

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -5,7 +5,7 @@ export const auth = defineAuth({
     email: {
       verificationEmailStyle: "CODE",
       verificationEmailSubject: "Welcome to the AI-Powered Recipe Generator!",
-      verificationEmailBody: (createCode) =&gt;
+      verificationEmailBody: (createCode) =>
         `Use this code to confirm your account: ${createCode()}`,
     },
   },
