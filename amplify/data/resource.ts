aa8a041ad83b6cbf150a7cc1efe08ec04ea7d61a
Ranjan Shrestha <ranjan@Pro.lan>
2025-08-14 17:23:34 -0500

    added amplify packages

---
+++ amplify/data/resource.ts
@@ -0,0 +1,3 @@
+import { type ClientSchema, a, defineData } from "@aws-amplify/backend"; const schema = a.schema({ BedrockResponse: a.customType({ body: a.string(), error: a.string(), }), askBedrock: a .query() .arguments({ ingredients: a.string().array() }) .returns(a.ref("BedrockResponse")) .authorization((allow) =&gt; [allow.authenticated()]) .handler( a.handler.custom({ entry: "./bedrock.js", dataSource: "bedrockDS" }) ), }); export type Schema = ClientSchema
+
+ ; export const data = defineData({ schema, authorizationModes: { defaultAuthorizationMode: "apiKey", apiKeyAuthorizationMode: { expiresInDays: 30, }, }, });
